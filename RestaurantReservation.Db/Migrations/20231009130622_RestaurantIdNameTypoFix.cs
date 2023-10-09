bf5db96379a7c71f0f2389a807e469a9282b7e06
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-10-09 16:07:47 +0300

    Add typo fix migration

---
+++ RestaurantReservation.Db/Migrations/20231009130622_RestaurantIdNameTypoFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestaurantIdNameTypoFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "restaurants_id",
+                table: "Restaurants",
+                newName: "restaurant_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "restaurant_id",
+                table: "Restaurants",
+                newName: "restaurants_id");
+        }
+    }
+}
