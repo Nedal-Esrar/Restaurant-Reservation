7eb04d5bbae7aae24ca7908dfa1d42f2cd6f6ff5
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-10-09 13:33:50 +0300

    Add Customer Create/Update/Delete methods

--- RestaurantReservation/RestaurantReservationRepository.cs
+++ RestaurantReservation/RestaurantReservationRepository.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using RestaurantReservation.Db;
+using RestaurantReservation.Db.Models;
 
 namespace RestaurantReservation;
 
@@ -9,5 +11,41 @@ public class RestaurantReservationRepository
   public RestaurantReservationRepository(RestaurantReservationDbContext context)
   {
     _context = context ?? throw new ArgumentNullException(nameof(context));
+  }
+  
+  public async Task CreateCustomerAsync(Customer customer)
+  {
+    if (customer is null) throw new ArgumentNullException(nameof(customer));
+
+    await _context.Customers.AddAsync(customer);
+
+    await _context.SaveChangesAsync();
+  }
+
+  public async Task UpdateCustomerAsync(Customer customer)
+  {
+    if (customer is null) throw new ArgumentNullException(nameof(customer));
+
+    if (!await DoesCustomerExistAsync(customer.CustomerId))
+      throw new NotFoundException(StandardMessages.GenerateNotFoundMessage("Customer", customer.CustomerId));
+
+    _context.Customers.Update(customer);
+
+    await _context.SaveChangesAsync();
+  }
+
+  public async Task<bool> DoesCustomerExistAsync(int id)
+  {
+    return await _context.Customers.AnyAsync(c => c.CustomerId == id);
+  }
+
+  public async Task DeleteCustomerAsync(int id)
+  {
+    var customer = await _context.Customers.FindAsync(id) ??
+                   throw new NotFoundException(StandardMessages.GenerateNotFoundMessage("Customer", id));
+
+    _context.Customers.Remove(customer);
+
+    await _context.SaveChangesAsync();
   }
 }
