c8d61ee3dce89b0d5eeef47d9229a03115ecc71a
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-10-09 13:31:18 +0300

    Add DbSets for each model

--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models;
 
 namespace RestaurantReservation.Db;
 
@@ -8,6 +9,22 @@ public class RestaurantReservationDbContext : DbContext
   public RestaurantReservationDbContext(DbContextOptions<RestaurantReservationDbContext> options) : base(options)
   {
   }
+  
+  public DbSet<Customer> Customers { get; set; }
+
+  public DbSet<Employee> Employees { get; set; }
+
+  public DbSet<MenuItem> MenuItems { get; set; }
+
+  public DbSet<Order> Orders { get; set; }
+
+  public DbSet<OrderItem> OrderItems { get; set; }
+
+  public DbSet<Reservation> Reservations { get; set; }
+
+  public DbSet<Restaurant> Restaurants { get; set; }
+
+  public DbSet<Table> Tables { get; set; }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
