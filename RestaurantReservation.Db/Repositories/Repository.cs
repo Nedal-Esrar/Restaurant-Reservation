5bf3018a0b755f1f36ea704bc920d2bdeee98d83
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-10-11 10:45:05 +0300

    Add `Repository` abstract class to eliminate redundant code in Repositories
    Repository class contains `CreateAsync`, `UpdateAsync`, `DeleteAsync`, and `IsExistAsync` methods.

---
+++ RestaurantReservation.Db/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.Db.Repositories;
+
+public abstract class Repository<TEntity> where TEntity : Entity
+{
+  protected readonly RestaurantReservationDbContext Context;
+
+  protected Repository(RestaurantReservationDbContext context)
+  {
+    Context = context ?? throw new ArgumentNullException(nameof(context));
+  }
+
+  public async Task CreateAsync(TEntity entity)
+  {
+    if (entity is null) throw new ArgumentNullException();
+
+    await Context.Set<TEntity>().AddAsync(entity);
+
+    await Context.SaveChangesAsync();
+  }
+  
+  public async Task UpdateAsync(TEntity entity)
+  {
+    if (entity is null) throw new ArgumentNullException();
+
+    if (!await IsExistAsync(entity.Id))
+      throw new NotFoundException(StandardMessages.GenerateNotFoundMessage(nameof(entity), entity.Id));
+
+    Context.Set<TEntity>().Update(entity);
+
+    await Context.SaveChangesAsync();
+  }
+
+  public async Task DeleteAsync(int id)
+  {
+    var entity = await Context.Set<TEntity>().FindAsync(id) ??
+                 throw new NotFoundException(StandardMessages.GenerateNotFoundMessage("entity", id));
+
+    Context.Set<TEntity>().Remove(entity);
+
+    await Context.SaveChangesAsync();
+  }
+
+  public async Task<bool> IsExistAsync(int id)
+  {
+    return await Context.Set<TEntity>().AnyAsync(e => e.Id == id);
+  }
+}
