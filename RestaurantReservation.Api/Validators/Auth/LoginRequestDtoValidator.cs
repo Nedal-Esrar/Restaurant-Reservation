38cd6ac45823b2893b783a615aa238f83a96c05e
Nedal-Esrar <nedalesrarahmad@gmail.com>
2023-12-10 13:25:41 +0200

    Add `ValidationMessages`

---
+++ RestaurantReservation.Api/Validators/Auth/LoginRequestDtoValidator.cs
@@ -0,0 +1,9 @@
+using FluentValidation;
+using RestaurantReservation.Api.Models.Auth;
+
+namespace RestaurantReservation.Api.Validators.Auth;
+
+public class LoginRequestDtoValidator : AbstractValidator<LoginRequestDto>
+{
+  
+}
---
+++ RestaurantReservation.Api/Validators/ValidationMessages.cs
@@ -0,0 +1,12 @@
+namespace RestaurantReservation.Api.Validators;
+
+public class ValidationMessages
+{
+  public const string PasswordUpperCase = "Password must include upper case letters.";
+
+  public const string PasswordLowerCase = "Password must include lower case letters.";
+
+  public const string PasswordDigits = "Password must include digits";
+
+  public const string PasswordSpecialCharacters = "Password must include special characters";
+}
