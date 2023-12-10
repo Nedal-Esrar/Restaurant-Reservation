6492c1e07a87db15dba3be24fe970698af2ce5c2
Nedal-Esrar <nedalesrarahmad@gmail.com>
2023-12-10 16:04:05 +0200

    Remove web api defaults, configuring services at app entry point, and make JWT token lifetime 60 minutes

--- RestaurantReservation.Api/Program.cs
+++ RestaurantReservation.Api/Program.cs
@@ -1,13 +1,140 @@
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using RestaurantReservation.Api.Auth;
+using RestaurantReservation.Api.Configurations;
+using RestaurantReservation.Api.Interfaces;
+using RestaurantReservation.Api.Middlewares;
+using RestaurantReservation.Db;
+using RestaurantReservation.Db.Interfaces;
+using RestaurantReservation.Db.Repositories;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+  .MinimumLevel.Error()
+  .WriteTo.File("logs/errors.txt", rollingInterval: RollingInterval.Day)
+  .CreateLogger();
+
+builder.Host.UseSerilog();
+
+var services = builder.Services;
+
+services.AddEndpointsApiExplorer();
+
+services.AddSwaggerGen(setup =>
+{
+  setup.SwaggerDoc("v1", new OpenApiInfo{ Title = "Restaurant Reservation API", Version = "v1" });
+
+  var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+  
+  var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+  
+  setup.IncludeXmlComments(xmlCommentsFullPath);
+
+  setup.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+  {
+    In = ParameterLocation.Header,
+    Description = "Please enter a valid token",
+    Name = "Authorization",
+    Type = SecuritySchemeType.Http,
+    BearerFormat = "JWT",
+    Scheme = "Bearer"
+  });
+  
+  setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+  {
+    {
+      new OpenApiSecurityScheme
+      {
+        Reference = new OpenApiReference
+        {
+          Type = ReferenceType.SecurityScheme,
+          Id = "Bearer"
+        }
+      },
+      Array.Empty<string>()
+    }
+  });
+});
+
+services.AddDbContext<RestaurantReservationDbContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("RestaurantReservationCoreDb")));
+
+services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+services.AddControllers()
+  .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles)
+  .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+  .AddFluentValidation(config =>
+  {
+    config.ImplicitlyValidateChildProperties = true;
+    config.ImplicitlyValidateRootCollectionElements = true;
+    config.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+  });;
+
+services.Configure<JwtAuthConfig>(builder.Configuration.GetSection(nameof(JwtAuthConfig)));
+
+services.AddAuthentication(options =>
+{
+  options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+  var config = builder.Configuration.GetSection(nameof(JwtAuthConfig)).Get<JwtAuthConfig>()!;
+
+  var key = Encoding.UTF8.GetBytes(config.Key);
+
+  options.TokenValidationParameters = new TokenValidationParameters
+  {
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidateLifetime = true,
+    ValidateIssuerSigningKey = true,
+    ValidIssuer = config.Issuer,
+    ValidAudience = config.Audience,
+    IssuerSigningKey = new SymmetricSecurityKey(key),
+    ClockSkew = TimeSpan.Zero
+  };
+});
+
+services.AddAuthorization();
+
+services.AddScoped<GlobalExceptionHandlingMiddleware>();
+
+services.AddApiVersioning(setupAction =>
+{
+  setupAction.AssumeDefaultVersionWhenUnspecified = true;
+
+  setupAction.DefaultApiVersion = new ApiVersion(1, 0);
+
+  setupAction.ReportApiVersions = true;
+});
+
+services.AddScoped<ICustomerRepository, CustomerRepository>()
+  .AddScoped<IEmployeeRepository, EmployeeRepository>()
+  .AddScoped<IMenuItemRepository, MenuItemRepository>()
+  .AddScoped<IOrderRepository, OrderRepository>()
+  .AddScoped<IOrderItemRepository, OrderItemRepository>()
+  .AddScoped<IReservationRepository, ReservationRepository>()
+  .AddScoped<IRestaurantRepository, RestaurantRepository>()
+  .AddScoped<ITableRepository, TableRepository>()
+  .AddScoped<IPasswordHasher, PasswordHasher>()
+  .AddScoped<IUserRepository, UserRepository>()
+  .AddScoped<IRoleRepository, RoleRepository>()
+  .AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
@@ -16,29 +143,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-  "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
 
-app.MapGet("/weatherforecast", () =>
-  {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-          DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-          Random.Shared.Next(-20, 55),
-          summaries[Random.Shared.Next(summaries.Length)]
-        ))
-      .ToArray();
-    return forecast;
-  })
-  .WithName("GetWeatherForecast")
-  .WithOpenApi();
+app.UseAuthorization();
+
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
